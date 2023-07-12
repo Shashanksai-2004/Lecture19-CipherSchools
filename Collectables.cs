1b5ff86069656f3a8b5bed60fd7324a91217f7bb
Shashanksai-2004 <103549184+Shashanksai-2004@users.noreply.github.com>
2023-07-12 12:42:23 +0530

    Add files via upload

---
+++ Collectables.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectables : MonoBehaviour
+{
+   private void OnTriggerEnter(Collider other)
+   {
+        if(other.gameObject.tag == "Player")
+        {
+            //add score
+            ScoreManager.instance.Addscore(5);
+            
+            Invoke("DestroyCollectables", 0.2f);
+        }
+   }
+
+   void DestroyCollectables()
+   {
+    Destroy(this.gameObject);
+   }
+}
